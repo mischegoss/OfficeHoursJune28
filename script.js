4132d7b0927a8ea50fab1266a96cbca9b0e342c3
Tamar <mischegoss@gmail.com>
2020-06-28 23:31:50 -0400

    Add notes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Always start off with this and wrap it around all of your Javascript
 $( document ).ready( function() {
 
 
@@ -38,7 +40,10 @@ function checkTime() {
               finalTime = parseInt(cutTime.charAt(1))
           }
         
-//It would be better to use class here but this  works
+//It would be better to use class here but this works and show you how to add
+//css directly. Note: Please use a css sheet, not inline CSS like I did in my
+//HTML.
+
           if (cutTime < momentTime) {
             var checkedString = $(this).children(".text-value").text()
           
@@ -54,7 +59,12 @@ function checkTime() {
 
       })
 
+//Here is the basic putting time in.
+//I looked up the format from Moment
 
+var mytime = moment().format("dddd, MMMM, Do");
+
+$(".moment-time").text( mytime);
 
 
 
@@ -67,3 +77,20 @@ checkTime()
 
 
 
+/* Alternate ways of grabbing the time:
+* You could use the area where you make show your time a button which makes it something with an 
+* input. By making it a button (that doesn't do anything on click) you can use value and grab it.
+* You could also make this a feature by making the button do something. This is a bit of a hack
+* but would save you a few lines of code.
+
+*  You could also assign each text area an id and then use the id. To do this it would be best
+* to give it name like text-8 and then split it on the '-' to get the 8. This also requires 
+* string manipulation */
+
+/* I mentioned using parseInt to make sure you had integers but you can also test for type
+ * using .type() in JQuery */
+ 
+
+ /* For local storage the most important thing to remember is that every text-area needs to 
+  * have its own key so you can put it back in the right place. If you don't you will just
+  * overwrite what you previously saved. I gave you a hint above */ 
